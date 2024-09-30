3f3303cc1ec9ab2c3464021ec1b1b65a273eb203
Marcos Vinicius <contato.marcoscandido@gmail.com>
2024-09-30 18:04:33 -0300

    Construindo a classe de serviço do domímio de lincensa

--- src/main/java/com/optimagrowth/licensingservice/model/License.java
+++ src/main/java/com/optimagrowth/licensingservice/model/License.java
@@ -24,5 +24,62 @@ public class License {
 	 private String organizationId;
 	 private String productName;
 	 private String licenseType;
+	 
+	 
+	 
+	 
+	public License() {
+		super();
+	}
+	public License(int id, String licenseId, String description, String organizationId, String productName,
+			String licenseType) {
+		super();
+		this.id = id;
+		this.licenseId = licenseId;
+		this.description = description;
+		this.organizationId = organizationId;
+		this.productName = productName;
+		this.licenseType = licenseType;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLicenseId() {
+		return licenseId;
+	}
+	public void setLicenseId(String licenseId) {
+		this.licenseId = licenseId;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getOrganizationId() {
+		return organizationId;
+	}
+	public void setOrganizationId(String organizationId) {
+		this.organizationId = organizationId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getLicenseType() {
+		return licenseType;
+	}
+	public void setLicenseType(String licenseType) {
+		this.licenseType = licenseType;
+	}
+	 
+	 
+	 
+	 
 
 }
