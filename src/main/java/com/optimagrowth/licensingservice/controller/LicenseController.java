fe71fa5d47611328054e007222c1dc8b4fdd3793
Marcos Vinicius <contato.marcoscandido@gmail.com>
2024-09-30 18:49:27 -0300

    criando a classe de controller para o dominio de licensa

--- src/main/java/com/optimagrowth/licensingservice/controller/LicenseController.java
+++ src/main/java/com/optimagrowth/licensingservice/controller/LicenseController.java
@@ -1,8 +1,14 @@
 package com.optimagrowth.licensingservice.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.optimagrowth.licensingservice.model.License;
+import com.optimagrowth.licensingservice.service.LicenseService;
 
 
 /*
@@ -17,8 +23,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="v1/organization/{organizationId}/license")
 public class LicenseController {
 	
+	@Autowired
+	private LicenseService lincenseService;
 	
+	LicenseController(LicenseService lincenseService){
+		
+		this.lincenseService = lincenseService;
+	}
 	
+	@GetMapping("/{licenseId}")
+	public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId , @PathVariable
+			("licenseId") String licenseId){
+		
+		License license = lincenseService.getLicense(licenseId, organizationId);
+		return ResponseEntity.ok(license);
+		
+	}
 	
 
 }
